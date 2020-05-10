5c682317507b6d976baaffefb18814acb5868188
fabiangunzinger <fa.gunzinger@gmail.com>
2020-05-10 16:41:32 +0200

    Vastly improving data lookup
    Using english database.

---
+++ foods/clean.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+
+def read_raw():
+
+    def strip_strings(df, columns):
+        """
+        Remove leading and trailing whitespace from strings.
+        """
+        df = df.copy()
+        def strip(col):
+            return col.str.strip()
+
+        df[columns] = df[columns].apply(strip)
+        return df
+
+    def str_to_numeric(df, columns):
+        """
+        Convert string columns to numeric.
+        """
+        df = df.copy()
+        def converter(col):
+            return pd.to_numeric(col, errors='coerce')
+
+        df[columns] = df[columns].apply(converter)
+        return df
+
+    def str_to_lower(df, columns):
+        """
+        Convert string columns to lower-case.
+        """
+        df = df.copy()
+        def converter(col):
+            return col.str.lower()
+
+        df[columns] = df[columns].apply(converter)
+        return df
+
+    columns = {
+        'Name': 'name',
+        'Matrix unit': 'units',
+        'Energy, kilocalories (kcal)': 'calories',
+        'Fat, total (g)': 'fat',
+        'Carbohydrates, available (g)': 'carbs',
+        'Sugars (g)': 'sugar',
+        'Dietary fibres (g)': 'fiber',
+        'Protein (g)': 'protein',
+    }
+
+    file = './data/Swiss-food-composition-database-V6.1.xlsx'
+
+    df = (
+        pd.read_excel(file, skiprows=2, usecols=columns)
+        .rename(columns, axis=1)
+        .pipe(str_to_numeric, ['fat', 'carbs', 'fiber', 'protein'])
+        .pipe(str_to_lower, ['name'])
+        .pipe(strip_strings, ['name'])
+        .dropna(how='any'))
+    return df
+
+
+def find_foods_item(data, item):
+    return data[data.name.str.contains(item)]
+
---
+++ foods/meals.py
@@ -0,0 +1,84 @@
+fabian_meals = {
+
+    'Lunch': {
+        'cheese': 0.7,
+        'coconut fat': 0,
+        'eggs': 0,
+        'leek': 0,
+        'mushrooms': 0,
+        'spinach': 0.5,
+
+        'avocado': 1.6,
+        'lettuce': 0.5,
+        'nuts and seed mix': 0.3,
+        'olive oil': 0,
+        'red pepper': 0,
+    },
+
+    'Snack': {
+        'nuts and seed mix': 1,
+        'cheese': 0,
+    },
+
+    'Supper': {
+        'chicken': 2,
+        'vegetables': 2,
+        'coconut fat': .3,
+    }
+}
+
+
+molly_meals = {
+
+    'Breakfast': {
+        'cheese': 0.5,
+        'eggs': 1.3,
+        'spinach': 0.5,
+    },
+
+    'Lunch': {
+        'cottage cheese': 2.5,
+        'spinach': 0.5,
+        'avocado': 1.6,
+        'red pepper': 1,
+        'lettuce': 1.5,
+        'nuts and seed mix': .5,
+        'flaxseed oil': .2,
+    },
+
+    'Snack': {
+        'nuts and seed mix': 0.5,
+        'nature yoghurt': 1,
+        'cheese': 0.5,
+        'half-and-half cream': 0.3,
+    },
+
+    'Supper': {
+        'eggs': 1.3,
+        'vegetables': 2,
+    }
+}
+
+
+
+
+paps_meals = {
+
+    'Zmittag': {
+        'oats': 0.5,
+        'milk': 1.5,
+        'nature yoghurt': 0.8,
+        'mocca yoghurt': 1.5,
+        'nuts': 0.4,
+        'fig': 0.2
+    },
+
+    'Znacht': {
+        'rice': 1.5,
+        'dark chocolate': .15,
+        'cheese': 0.6,
+        'eggs': 0.6,
+        'beer': 5
+    },
+
+}
