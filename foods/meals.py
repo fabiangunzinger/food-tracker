eb64b8f19fa419911f1748b71f444f2dcaadd8c0
fabiangunzinger <fa.gunzinger@gmail.com>
2020-05-17 20:00:01 +0200

    Update M and F meals

--- foods/meals.py
+++ foods/meals.py
@@ -6,33 +6,28 @@ fabian_meals = {
         'mushroom, stewed (without addition of fat and salt)': 1,
         'spinach, steamed (without addition of salt)': 0.6,
 
-        'iceberg lettuce, raw': 0.5,
+        'iceberg lettuce, raw': 1,
         'avocado, fresh': 1.6,
         'bell pepper, red, raw': 0.3,
-        'seeds, kernels, nuts (average), unsalted': 0.25,
         'olive oil': 0.2,
         'vinegar': 0.1,
     },
 
-    'Snack': {
-        'coconut fat': 0.3,
-        'cocoa powder': 0.05,
-        'coffee, black, no sugar added': 1,
-        'brazil nut': 0.5
-    },
+    # 'Snack': {
+    #     'coconut fat': 0.2,
+    #     'cocoa powder': 0.05,
+    #     'coffee, black, no sugar added': 1,
+    #     'brazil nut': 0.5
+    # },
 
     'Supper': {
-        'iceberg lettuce, raw': 0.3,
-        'avocado, fresh': 0.8,
-        'bell pepper, red, raw': 0.3,
-
         'chicken, leg with skin, roasted (without addition of fat and salt)': 2,
-
-        'coconut fat': 0.2,
+        'scrambled eggs, prepared': 1.2,
+        'coconut fat': 0.4,
         'kohlrabi, steamed (without addition of salt)': 0.5,
         'carrot, steamed (without addition of salt)': 0.5,
         'cauliflower, steamed (without addition of salt)': 0.5,
-        'vegetables (average), raw': 1
+        'vegetables (average), raw': 0.5
     }
 }
 
@@ -40,32 +35,33 @@ fabian_meals = {
 molly_meals = {
 
     'Breakfast': {
-        'cheese': 0.5,
-        'eggs': 1.3,
-        'spinach': 0.5,
+        'scrambled eggs, prepared': 1.4,
+        'mushroom, stewed (without addition of fat and salt)': 0.4,
+        'spinach, steamed (without addition of salt)': 0.3,
     },
 
     'Lunch': {
-        'cottage cheese': 2.5,
-        'spinach': 0.5,
-        'avocado': 1.6,
-        'red pepper': 1,
-        'lettuce': 1.5,
-        'nuts and seed mix': .5,
-        'flaxseed oil': .2,
+
+        'iceberg lettuce, raw': 0.3,
+        'avocado, fresh': 0.8,
+        'bell pepper, red, raw': 0.3,
+        'cottage cheese': 2,
+        'seeds, kernels, nuts (average), unsalted': 0.1,
+        'olive oil': 0.2,
     },
 
+
     'Snack': {
-        'nuts and seed mix': 0.5,
-        'nature yoghurt': 1,
-        'cheese': 0.5,
-        'half-and-half cream': 0.3,
+        'greyerzer, at least 45% fidm': 1,
+        'nuts and seed mix': 1,
+        'cream (average)': 0.3,
     },
 
     'Supper': {
-        'eggs': 1.3,
-        'vegetables': 2,
+        'scrambled eggs, prepared': 1.3,
+        'egetables (average), raw': 2,
     }
+
 }
 
 
