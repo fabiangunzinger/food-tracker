ae92b9ddd7eca1255446d27311532a8f034699b8
fabiangunzinger <fa.gunzinger@gmail.com>
2020-09-14 17:50:36 +0100

    Save changes.{

--- foods/meals.py
+++ foods/meals.py
@@ -1,53 +1,208 @@
+sep14_2020 = {
+
+    'breakfast': {    
+        'butter of choice': 0.1,
+        'egg, raw': 3,
+        'pip & nut almond butter': 0.3,
+        'alpro coconut milk': 4,
+        'coco powder': 0.02,
+        'avocado, fresh': 1.8,        
+    },
+    
+    'lunch': {        
+        'beef mince (tesco finest)': 2.5,
+        'mayonnaise': 0.3,
+
+        'coconut fat': 0.2,
+        'mushroom, stewed (without addition of fat and salt)': 1.5,
+        'spinach, steamed (without addition of salt)': 0.6,
+        
+        'iceberg lettuce, raw': 0.4,
+        'avocado, fresh': 1.8,
+        'olive oil': 0.2,
+    },
+    
+    'supper': {
+        'tesco chicken thighs and drummsticks': 1.5,
+        'mayonnaise': 0.3
+        
+    },
+    
+}
+
+
+
+sep07_2020 = {
+
+    'breakfast': {    
+        'butter of choice': 0.1,
+        'egg, raw': 1.8,
+        'pip & nut almond butter': 0.15,
+        'pride coconut milk': 4,
+        'coco powder': 0.02
+    },
+    
+    'lunch': {        
+        'coconut fat': 0.2,
+        'beef mince (tesco finest)': 2.5,
+        'mushroom, stewed (without addition of fat and salt)': 1.5, # 4 medium
+        'spinach, steamed (without addition of salt)': 0.6,   # handfull
+        'iceberg lettuce, raw': 0.4, # lettuce mix
+        'avocado, fresh': 1.8, # one large avocados
+        'olive oil': 0.2,
+    },
+    
+    'supper': {
+        'avocado, fresh': 0.9,
+        'tesco chicken thighs and drummsticks': 2.5,
+        'iceberg lettuce, raw': 0.4, # lettuce mix        
+    },
+    
+}
+
+
+aug29_2020 = {
+
+    'Drinks': {
+    },
+    
+    'breakfast': {    
+        'butter of choice': 0.3,
+        'salmon, smoked': 0.4,
+        'egg, raw': 3,
+        'avocado, fresh': 1.8,
+        'olive oil': 0.2,
+    },
+    
+    'lunch': {        
+        'coconut fat': 0.2,
+        'beef mince (tesco finest)': 2.5,   # 2 small
+        'mushroom, stewed (without addition of fat and salt)': 1.5, # 4 medium
+        'spinach, steamed (without addition of salt)': 0.6,   # handfull
+        'iceberg lettuce, raw': 0.4, # lettuce mix
+        'avocado, fresh': 1.8, # one large avocados
+        'olive oil': 0.3,
+    },
+}
+
+
+
+aug9_2020 = {
+    
+    # Ate like this every day for about three weeks, with very good results. 
+    # Got lean, and felt very good.
+
+    'Drinks': {
+    },
+    
+    'breakfast': {        
+        'coconut fat': 0.2,
+        'egg, raw': 3,   # 2 small
+        'mushroom, stewed (without addition of fat and salt)': 1.5, # 4 medium
+        'spinach, steamed (without addition of salt)': 0.6,   # handfull
+        'iceberg lettuce, raw': 0.4, # lettuce mix
+        'avocado, fresh': 1.8, # one large avocados
+        'olive oil': 0.15,
+    },
+    
+    'lunch': {        
+        'coconut fat': 0.2,
+        'beef mince (tesco finest)': 2.5,   # 2 small
+        'mushroom, stewed (without addition of fat and salt)': 1.5, # 4 medium
+        'spinach, steamed (without addition of salt)': 0.6,   # handfull
+        'iceberg lettuce, raw': 0.4, # lettuce mix
+        'avocado, fresh': 1.8, # one large avocados
+        'olive oil': 0.3,
+    },
+}
+
+jul24_2020 = {
+
+    'Drinks': {
+        'spirulina': 0.03,
+        'kallo chicken stock cube (1 cube)': 1,
+    },
+    
+    'Lunch': {
+        'walnut': 0.15,
+        'brazil nut': 0.15,
+        'tesco bacon': 0.6,   # 3 slices
+        'cheddar': 0.65,
+        
+        'pork lard': 0.05,
+        'cooking butter': 0.15,
+        'egg, raw': 1.1,   # 2 small
+        'egg yolk': 0.55,   # 3 small
+        'mushroom, stewed (without addition of fat and salt)': 1.4, # 4 medium
+        'spinach, steamed (without addition of salt)': 0.65,   # handfull
+
+        'iceberg lettuce, raw': 0.4, # lettuce mix
+        'avocado, fresh': 0.9, # one small avocados
+        'olive oil': 0.15,
+    },
+    
+#     'Snack': {
+#         'brazil nut': 0.1,
+#         'walnut': 0.25,
+#         'cheddar': 0.45,
+#         'fish product, sardine in oil, drained': 0.75,
+#         'olive oil': 0.05 
+#     },
+
+#     'Supper': {    
+#         'pork lark': 0.05,
+#         'cooking butter': 0.15,
+#         'beef mince (tesco finest)': 1.8, 
+#         'broccoli, steamed (without addition of salt)': 0.5,
+#         'white cabbage, raw': 0.95,
+#         'avocado, fresh': 0.7,
+#     }
+}
+
+
 jul23_2020 = {
 
     'Drinks': {
         'spirulina': 0.03,
-        'kallo chicken stock cube': 0.66,
-    },
-        
-    'Breakfast': {
-        'pork lard': 0.15,
-        'cooking butter': 0.2,
-        'tesco bacon': 1,
-        'medium cheddar': 0.4,
-        'egg yolk': 1.5,
-        'egg white, raw': 0.5,   
-        'mushroom, stewed (without addition of fat and salt)': 0.4,
-        'spinach, steamed (without addition of salt)': 0.3,
+        'kallo chicken stock cube (1 cube)': 1,
     },
     
     'Lunch': {
-        'coconut fat': 0.2,
-        'cooking butter': 0.1,
-#         'egg, raw': 2.4,
-        'egg yolk': 1.5,
-        'egg white, raw': 0.5,       
-        'mushroom, stewed (without addition of fat and salt)': 1.2,
-        'tomato, raw': 0.6,
-        'spinach, steamed (without addition of salt)': 1.35,
+        'walnut': 0.2,
+        'brazil nut': 0.05,
+        'tesco bacon': 0.9,   # 3 slices
+        'cheddar': 0.55,
+        
+        'pork lard': 0.07,
+        'cooking butter': 0.28,
+        'onion, raw': 0.3,   # 1/4 of whole  
+        'egg, raw': 1.1,   # 2 small
+        'egg yolk': 0.55,   # 3 small
+        'mushroom, stewed (without addition of fat and salt)': 1.4, # 4 medium
+        'tomato, raw': 0.6,   # 5 plum tomatoes
+        'spinach, steamed (without addition of salt)': 0.65,   # handfull
 
         'iceberg lettuce, raw': 0.4, # lettuce mix
-        'avocado, fresh': 1, # one small avocados
+        'avocado, fresh': 0.9, # one small avocados
         'olive oil': 0.15,
     },
     
     'Snack': {
-        'macadamias': 0.2,
         'brazil nut': 0.1,
-#         'pip & nut almont butter': 0.5, # 2 tsps
+        'walnut': 0.25,
+        'cheddar': 0.45,
         'fish product, sardine in oil, drained': 0.75,
-#         'mayonnaise': 0.05,
-        
+        'olive oil': 0.05 
     },
 
-    'Supper': {        
-        'coconut fat': 0.2,
-        'cooking butter': 0.1,
-        'tesco wild salmon': 1, 
-        'broccoli, steamed (without addition of salt)': 1,
-        'spinach, steamed (without addition of salt)': 1,
-        'mushroom, stewed (without addition of fat and salt)': 1.2,
-        'avocado, fresh': 1,
+    'Supper': {    
+        'tesco bacon': 0.6,
+        'pork lark': 0.05,
+        'cooking butter': 0.32,
+        'beef mince (tesco finest)': 1.2, 
+        'broccoli, steamed (without addition of salt)': 0.5,
+        'white cabbage, raw': 0.95,
+        'avocado, fresh': 0.7,
     }
 }
 
